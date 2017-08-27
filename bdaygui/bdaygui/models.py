19aeee754d6eafa881e1602e416555f2fa84b1be
Kirill Borisov <lensvol@gmail.com>
2017-08-27 12:53:05 +0300

    Swith to using separate table for importance values.

--- bdaygui/bdaygui/models.py
+++ bdaygui/bdaygui/models.py
@@ -3,6 +3,25 @@
 from django.db import models
 
 
+def get_default_importance_value():
+    return Importance.objects.get_or_create(code=2, name=u'Не нужно поздравлять')[0].id
+
+
+class Importance(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name=u'ID')
+    code = models.IntegerField(verbose_name=u'Код')
+    name = models.CharField(max_length=16, verbose_name=u'Наименование')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = u'Степень важности'
+        verbose_name_plural = u'Степени важности'
+        db_table = 'importance'
+        ordering = ['code']
+
+
 class Birthday(models.Model):
     id = models.AutoField(primary_key=True, verbose_name=u'ID')
     charid = models.IntegerField(verbose_name=u'ID AniDB')
@@ -12,7 +31,17 @@ class Birthday(models.Model):
     series = models.TextField(verbose_name=u'Произведение')
     original_name = models.TextField(verbose_name=u'Имя на японском')
     photo = models.TextField(verbose_name=u'Фотография')
-    important = models.BooleanField(verbose_name=u'Важный персонаж')
+    importance = models.ForeignKey(Importance, null=False,
+                                   default=get_default_importance_value,
+                                   verbose_name=u'Важность персонажа')
+
+    def __unicode__(self):
+        return u'Birthday: {0} [#{1}] on {2}.{3}'.format(
+            self.name,
+            self.charid,
+            self.day,
+            self.month,
+        )
 
     class Meta:
         unique_together = ('name', 'day', 'month')
